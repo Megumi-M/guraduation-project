983afa3387b38073f77daa1ba495cf097048b3b6
Megumi Imada <m0e5g2u1imd@gmail.com>
2019-02-20 14:17:36 +0000

    Letteropener修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,3 +89,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+ config.action_mailer.default_url_options = { host: 'gentle-beach-90680.herokuapp.com'}
+ config.action_mailer.delivery_method = :letter_opener_web
