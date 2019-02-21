2c3c3b15b6ce898e0c8d05dbeaea72b82e043fce
Megumi Imada <m0e5g2u1imd@gmail.com>
2019-02-21 15:57:46 +0000

    20190221

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :users, :only => [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'lessons#index' #ログイン後に遷移する画面
-  if Rails.env.development?
   mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
   
