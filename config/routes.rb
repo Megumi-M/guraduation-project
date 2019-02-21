ce4f7ac80b3bee6d9aa58dcec95302ac31001e89
Megumi Imada <m0e5g2u1imd@gmail.com>
2019-02-21 16:10:53 +0000

    rootes.rb修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :users, :only => [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'lessons#index' #ログイン後に遷移する画面
+  root to: 'lessons#index' #ログイン後に遷移する画面
+  
   mount LetterOpenerWeb::Engine, at: "/letter_opener"
   
   resources :lessons do
